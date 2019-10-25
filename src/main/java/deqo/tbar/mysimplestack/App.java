b14f043360c66acabc909292892524a8eb4e228c
Tim-974 <timothee.barde@laposte.net>
2019-10-25 16:35:06 +0200

    exo 2 question 3

---
+++ src/main/java/deqo/tbar/mysimplestack/App.java
@@ -0,0 +1,13 @@
+package deqo.tbar.mysimplestack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
